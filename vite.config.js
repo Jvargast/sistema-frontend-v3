23260302bb7a1732aa8c4143d275dc0f2ee1d91c
jvargast3 <jvargast3@correo.uss.cl>
2025-03-27 19:59:17 -0300

    cambios deploy

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,35 @@ import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 
-// https://vite.dev/config/
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [react()],
-  optimizeDeps: {
-    include: ["swapy"],
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
-    proxy: {
-      "/api": {
-        target: "http://192.168.1.121:8080",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
       },
     },
-  },
+    plugins: [react()],
+    optimizeDeps: {
+      include: ["swapy"],
+    },
+    server: isDev
+      ? {
+          host: "0.0.0.0",
+          port: 3000,
+          strictPort: true,
+          proxy: {
+            "/api": {
+              target: "http://192.168.1.121:8080", // local backend
+              changeOrigin: true,
+              secure: false,
+            },
+          },
+        }
+      : undefined, // No necesitas `server` en producción (lo ignora)
+    build: {
+      outDir: "dist",
+    },
+  };
 });
